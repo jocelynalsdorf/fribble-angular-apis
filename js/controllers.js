54bea464051337a2df690ae2b1ddba5515d1617b
jocelynalsdorf <jfrodsla@gmail.com>
2015-10-06 11:25:00 -0700

    fix navbar styles; basic html structure for shot page

--- js/controllers.js
+++ js/controllers.js
@@ -15,6 +15,10 @@ app.controller("ShotsListCtrl", function ShotsListCtrl($scope, $http, $routePara
 });
 
 app.controller("ShotsCtrl", function ShotsListCtrl($scope, $routeParams, $http) {
-     console.log($routeParams); 
+  var id = $routeParams.id
+  $http.get('https://api.dribbble.com/v1/shots/' + id + '?access_token=e348fac8697cca93da8077ca25156265785479e8b9f35425b4369cbd3ddf4a89').success(function(data){
+    $scope.shot = data;
+     console.log(data);
+  });
 
 });
